7dabc0b8fe90260155df5ccffdf66c8c217bbaf0
mikesaurio <mikesaurio@gmail.com>
2015-07-30 16:50:54 -0500

    se reagalan votos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,19 +3,31 @@ class HomeController < ApplicationController
     @users = User.all
     if current_user
       @user = User.where(name: current_user.name).last
-      @users = User.where.not(id: current_user)
+      #@users = User.where.not(id: current_user)
     end
     
     if params[:get]
+      if params[:get][:category]
        respues = params[:get][:category]
        poll = Poll.create(name: "trolling", reply: respues)
         if poll.save
           flash[:success] = "poll save"
-          a = @user.active_votes -= 1
+          @user.active_votes -= 1
           @user.save
         else
           flash[:success] = "Error"
         end
+      end
+       if params[:get][:poll]
+          respues = params[:get][:poll]
+          a = User.find(respues)
+          a.active_votes += 1
+          if a.save
+             flash[:success] = "vote delegate"
+          else
+            flash[:success] = "Error"
+          end
+       end
     end
 
   end
