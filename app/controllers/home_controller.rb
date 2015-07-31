5247eb88933c52c847ff555e1af2bbf187226252
mikesaurio <mikesaurio@gmail.com>
2015-07-30 20:39:01 -0500

    se elimina uno a no

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,51 +9,54 @@ class HomeController < ApplicationController
       @relations = Relationpoll.where(user: current_user.uid)
     end
     
-    if params[:get]
-      if params[:get][:category]
+      if params[:get]#1
+      if params[:get][:category]#2
        respues = params[:get][:category]
-       if Poll.where(name: "trolling",id_facebook: current_user.uid ).blank?
+       if Poll.where(name: "trolling", id_facebook: current_user.uid ).blank?#3
           poll = Poll.create(name: "trolling", reply: respues, id_facebook: current_user.uid)
-        if poll.save
+        if poll.save#4
           flash[:success] = "poll save"
           @user.active_votes = 0
           @user.save
-          @relations.each do |relacion|
-            if relacion.user == current_user.uid
+        
+          @relations.each do |relacion| #5
+           
+            if relacion.user == current_user.uid  #6
                poll = Poll.create(name: "trolling", reply: respues, id_facebook: current_user.uid)
-               pol.save
+               pol.save 
+            end #6
 
-            end
-          end
-        else
-          flash[:success] = "Error"
-        end
-      else
+          end#5
+
+        else#4
+          flash[:success] = "Error Save pol"
+        end#4
+
+      else#3
         flash[:success] = "already answered NOT SAVE"
-      end
+      end#3
+      end#2
 
-      end
-       if params[:get][:poll]
+       if params[:get][:poll]#7
           respues = params[:get][:poll]
           b = current_user
           a = User.find(respues)
-           if Relationpoll.where(user: b.uid, donator: a.uid).blank?
+           if Relationpoll.where(user: b.uid, donator: a.uid).blank?#8
             #quito mi voto
             b.active_votes -= 1
             b.save
             #aumento tu voto
             a.active_votes += 1
-            if a.save
+            if a.save#9
                flash[:success] = "vote delegate"
                   m = Relationpoll.create(user: b.uid, donator: a.uid, used: false)
                   m.save
              
-            else
+            else#9
               flash[:success] = "Error"
-            end
-        end
-       end
-    end
-
-  end
+            end#9
+       end#8
+    end#7
+end#1
+  end#show
 end
